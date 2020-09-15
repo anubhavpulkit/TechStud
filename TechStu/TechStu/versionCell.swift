3fcf6df96828231b3c5d8e52a3d25273b11ec744
Anubhav Singh <anubhavsingh@Anubhavs-MacBook-Air.local>
2020-09-15 19:09:26 +0530

    setting UI

---
+++ TechStu/TechStu/versionCell.swift
@@ -0,0 +1,27 @@
+//
+//  versionCell.swift
+//  TechStu
+//
+//  Created by Anubhav Singh on 15/09/20.
+//  Copyright © 2020 Swify. All rights reserved.
+//
+
+import UIKit
+
+class versionCell: UITableViewCell {
+
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

a65b50c8bfad2826aed6f6f4062d57fd11f0f1f9
Anubhav Singh <anubhavsingh@Anubhavs-MacBook-Air.local>
2020-09-15 14:29:02 +0530

    structure

---
+++ TechStu/TechStu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechStu
+//
+//  Created by Anubhav Singh on 15/09/20.
+//  Copyright © 2020 Swify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
